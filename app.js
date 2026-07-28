ea1b03234f3d3273bdddeaf2a5a53c130c66e4ab
FSiner <115860618+FSiner@users.noreply.github.com>
2026-07-28 18:07:59 +0900

    Add Firebase importmap to app.js
    Add importmap for Firebase libraries in app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+<script type="importmap">
+        {
+            "imports": {
+                "firebase/app": "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js",
+                "firebase/firestore": "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js",
+                "firebase/database": "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js"
+            }
+        }
+    </script>
